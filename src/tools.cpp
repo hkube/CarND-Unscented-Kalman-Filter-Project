699a5c051e39213df17b75c8d101e3f9697762f0
Harald Kube <harald.kube@gmx.de>
2018-01-31 23:43:22 +0100

    Re-format main.cpp + make CalculateRMSE a static method in class Tools + copy CMakeLists.txt to src

--- src/tools.cpp
+++ src/tools.cpp
@@ -5,14 +5,35 @@ using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
-Tools::Tools() {}
-
-Tools::~Tools() {}
-
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse.setZero();
+
+  if ((0 < estimations.size()) && (estimations.size() == ground_truth.size()))
+  {
+    //accumulate squared residuals
+    for(int i=0; i < estimations.size(); ++i){
+      VectorXd diff(4);
+      diff = estimations.at(i) - ground_truth.at(i);
+      diff = diff.array() * diff.array();
+      rmse += diff;
+    }
+
+    //calculate the mean
+    rmse = rmse / static_cast<double>(estimations.size());
+
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
+  }
+  else
+  {
+    std::cout << "DATA ERROR: estimations.size(): " << estimations.size() << "  ground_truth.size(): " << ground_truth.size() << std::endl;
+  }
+
+  return rmse;
 }
